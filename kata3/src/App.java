0fb2d5a719feb650833fd1fee65bf12c13ca0de3
RaúlGonzález71 <65398426+Rge071@users.noreply.github.com>
2020-12-09 11:47:03 +0000

    Update App.java

--- kata3/src/App.java
+++ kata3/src/App.java
@@ -5,10 +5,7 @@ import histogram.HistogramDisplay;
 public class App {
     public static void main(String[] args) {
         Histogram<String> histogram = new Histogram();
-        
-        histogram.increment("ulpgc.es");
-        histogram.increment("ull.es");
-        histogram.increment("dis.ulpgc.es");
+ 
         histogram.increment("ulpgc.es");
         histogram.increment("gmail.com");
         histogram.increment("hotmail.com");
@@ -16,16 +13,13 @@ public class App {
         histogram.increment("ulpgc.es");
         histogram.increment("gmail.com");
         histogram.increment("gmail.com");
-        histogram.increment("gmail.com");
-        histogram.increment("gmail.com");
         histogram.increment("hotmail.com");
         histogram.increment("ulpgc.es");
+        histogram.increment("dis.ulpgc.es");
         histogram.increment("ulpgc.es");
         histogram.increment("ull.es");
         histogram.increment("dis.ulpgc.es");
         histogram.increment("dis.ulpgc.es");
-        histogram.increment("dis.ulpgc.es");
-        histogram.increment("ulpgc.es");
         histogram.increment("ull.es");
         
         new HistogramDisplay("HISTOGRAM", histogram).execute();
