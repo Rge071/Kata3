e8458f758cd7464aa7f286690cfbe57b05eec162
RaúlGonzález71 <65398426+Rge071@users.noreply.github.com>
2020-12-09 11:45:07 +0000

    Add files via upload

---
+++ kata3/src/App.java
@@ -0,0 +1,33 @@
+
+import histogram.Histogram;
+import histogram.HistogramDisplay;
+
+public class App {
+    public static void main(String[] args) {
+        Histogram<String> histogram = new Histogram();
+        
+        histogram.increment("ulpgc.es");
+        histogram.increment("ull.es");
+        histogram.increment("dis.ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("gmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ull.es");
+        histogram.increment("dis.ulpgc.es");
+        histogram.increment("dis.ulpgc.es");
+        histogram.increment("dis.ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ull.es");
+        
+        new HistogramDisplay("HISTOGRAM", histogram).execute();
+    }
+}
